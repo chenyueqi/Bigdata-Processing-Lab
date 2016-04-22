bb876275386b85e9094ccf66ae01fdc8c56d0d24
陈越琦 <15905193328cyq@gmail.com>
2016-04-22 22:21:33 +0800

    ADD: Lab2

---
+++ Lab2/src/InvertedIndexReducer.java
@@ -0,0 +1,71 @@
+/*
+ * Reducer class for InvertedIndex Table
+ * Project Name: Inverted Index Table for Hadoop (Lab2)
+ * Group Name: What the f**k
+ * Created by: Yueqi Chen (Yueqichen.0x0@gmail.com)
+ * Time: 2016/4/22 21:59
+*/
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.util.*;
+
+public class InvertedIndexReducer extends Reducer<Text, IntWritable, Text, Text>
+{
+    static List<String> postingList = new ArrayList<String>();
+    static Text CurrentItem = new Text(" ");
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+    {
+	int sum = 0;
+	Text word1 = new Text();
+	Text word2 = new Text();
+	word1.set(key.toString().split("#")[0]);
+	String temp = new String();
+	temp = key.toString().split("#")[1];
+	for(IntWritable val : values)
+	    sum += val.get();
+
+	word2.set("<" + temp + "," + sum + ">");
+
+
+	if(!CurrentItem.equals(word1) && (!CurrentItem.equals(" ")))
+	{
+	    StringBuilder out = new StringBuilder();
+	    long cnt = 0;
+	    for(String p:postingList)
+	    {
+		out.append(p);
+		out.append(";");
+		cnt += Long.parseLong(p.substring(p.indexOf(",") +1, p.indexOf(">")));
+	    }
+	    out.append("<total," + cnt + ">.");
+	    if(cnt > 0)
+		context.write(CurrentItem, new Text(out.toString()));
+	    postingList = new ArrayList<String>();
+	}
+	CurrentItem = new Text(word1);
+	postingList.add(word2.toString());
+    }
+
+    public void cleanup(Context context) throws IOException, InterruptedException
+    {
+	StringBuilder out = new StringBuilder();
+
+	long cnt = 0;
+	for(String p:postingList)
+	{
+	    out.append(p);
+	    out.append(";");
+	    cnt += Long.parseLong(p.substring(p.indexOf(",") +1, p.indexOf(">")));
+
+	}
+	out.append("<total," + cnt + ">.");
+	if(cnt > 0)
+	    context.write(CurrentItem, new Text(out.toString()));
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This repository serves as a series of labs in the course Big Data Processing: Comprehensive Experiments (2016 Spring term), Department of Computer Science and Technology, Nanjing University.
 
 ## authors
-    Yueqi Chen(Yueqichen.0x0@gmail.com)
+    Yueqi Chen (Yueqichen.0x0@gmail.com)
 
 ## environment
     hadoop 0.20.205
