6c21b3c84c795dbdf41a3eeede6b7fec6024cd64
Wei Liu <liuwei13cs@smail.nju.edu.cn>
2016-05-07 18:05:42 +0800

    Added files via upload

---
+++ Lab3/src/InvertedIndex/NewPartitioner.java
@@ -0,0 +1,26 @@
+/*
+ * partitioner class for InvertedIndex Table
+ * Project Name: Inverted Index Table for Hadoop (Lab2)
+ * Group Name: What the f**k
+ * Created: Yueqi Chen (Yueqichen.0x0@gmail.com)
+ * Time: 2016/4/22 21:48
+*/
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
+
+public class NewPartitioner extends HashPartitioner<Text, IntWritable>
+{
+    @Override
+    public int getPartition(Text key, IntWritable value, int numReduceTasks)
+    {
+	String term = new String();
+	/*partitioner according to word instead of <word#filename>*/
+	term = key.toString().split("#")[0];
+	return super.getPartition(new Text(term), value, numReduceTasks);
+    }
+}
