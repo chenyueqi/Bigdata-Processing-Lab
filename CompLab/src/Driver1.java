1a5aaf3b59d6ff3a726ed24f6cd293bf65018c8e
Wei Liu <liuwei13cs@smail.nju.edu.cn>
2016-07-11 15:03:07 +0800

    Add files via upload

---
+++ CompLab/src/Driver1.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Driver1
+{
+	public static void main(String[] args) throws Exception
+	{
+		String[] stat1 ={args[0],args[1]};
+		Stat1.main(stat1);
+		String[] stat2 ={args[0],args[2]};
+		Stat2.main(stat2);
+		String[] stat3 ={args[0],args[3]};
+		Stat3.main(stat3);
+		String[] stat4 ={args[0],args[4]};
+		Stat4.main(stat4);
+	}
+}
